1b3653f1f94ec54b14ef132a56499c2361fc132a
Andrea Godsey <noirmenace101@gmail.com>
2024-10-23 16:18:58 -0400

    Web Sockets Notes

---
+++ api/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const buttonData = [
+  "red","green","orange"
+];
+
+app.get("/api/buttons/data", function (req,res) {
+    res.status(200).json(buttonData);
+});
+
+app.post("/api/buttons/data/new", function (req, res){
+    buttonData.push(req.body.value);
+    res.status(200).json(buttonData);
+})
+
+module.exports = app;
---
+++ views/app.js
@@ -0,0 +1,33 @@
+const graphRef = document.getElementById("graph");
+
+const chart = new Chart(graphRef, {
+    type: "bar",
+    data: {
+        labels: ["Green", "Red"],
+        datasets: [
+            { label: "Buttons", data: [2,1]}],
+    },
+});
+
+async function getData() {
+    const buttonReq = await fetch (`/api/buttons/data`);
+    const buttonData = await buttonReq.json();
+
+    const colors = {};
+    //{"red": 2, "green": 1}
+
+    for (let i = 0; i < buttonData.length; i++) {
+        const buttonColor = buttonData[i];
+        colors[buttonColor] ||=0;
+        colors[buttonColor]++;
+    }
+
+    chart.data = {
+        labels: Object.keys(colors),
+        datasets: [{ label: "Buttons", data: Object.values(colors)}],
+        
+    };
+    chart.update();
+}
+
+getData();
